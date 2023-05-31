557fc280c51d83dd6f41df41fa07c98abbbdb8e6
FerroSantiago <santiagoferroraffin@gmail.com>
2023-05-31 19:33:45 -0300

    update3

--- svelteApp/svelte.config.js
+++ svelteApp/svelte.config.js
@@ -1,14 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
-
+ 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			paths: {
-				base: '/intercatedra-fonoaud-ing-sistemas/'
-			}
-		})
-	}
+  kit: {
+    adapter: adapter({
+      runtime: 'nodejs18.x',
+    }),
+  },
 };
-
+ 
 export default config;
